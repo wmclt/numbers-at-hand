350c11fde98ee46aa14aa79a588fd5bdb781a967
Philip De Herdt <philip.de.herdt@acerta.be>
2020-08-23 15:51:19 +0200

    Remove todo's from code

--- plugin/popup.js
+++ plugin/popup.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var companyBtn = document.getElementById('btn-companynbr');
   var rszBtn = document.getElementById('btn-rsznbr');
   var ibanBtn = document.getElementById('btn-iban');
-  
+
   uuidBtn.addEventListener('click', function() { getRandomUuid(); }, false);
   uuidBtn.addEventListener('mouseout', function() { resetStatus(); }, false);
   rrnBtn.addEventListener('click', function() { getRandomRrnNumber(); }, false);
@@ -15,11 +15,11 @@ document.addEventListener('DOMContentLoaded', function() {
   rszBtn.addEventListener('mouseout', function() { resetStatus(); }, false);
   ibanBtn.addEventListener('click', function() { getRandomIban(); }, false);
   ibanBtn.addEventListener('mouseout', function() { resetStatus(); }, false);
-	
+
 	
   var stat = document.getElementById('status');
   var title = document.getElementById('title');
-  
+
   uuidBtn.innerHTML = chrome.i18n.getMessage("uuid");
   rrnBtn.innerHTML = chrome.i18n.getMessage("nationalIdentificationNumber");
   companyBtn.innerHTML = chrome.i18n.getMessage("companyNumber");
@@ -36,20 +36,20 @@ function getRandomUuid() {
 }
 //https://nl.wikipedia.org/wiki/RSZ-nummer
 function getRandomRszNumber() {
-	var base = randomIntFromInterval(1000,1999999); 
+	var base = randomIntFromInterval(1000,1999999);
 	var control = 96- (100*base)%97;
 	var rsznbr = ''.concat( 100 * base + control).padStart(9, "0");
-  
+
 	copy(rsznbr);
 	setStatus(rsznbr);
 }
 
 //https://nl.wikipedia.org/wiki/Kruispuntbank_van_Ondernemingen
 function getRandomCompanyNumber() {
-	var base = randomIntFromInterval(2000000,19999999); 
+	var base = randomIntFromInterval(2000000,19999999);
 	var control = 97 - base % 97;
 	var companynbr = ''.concat( 100 * base + control).padStart(10, "0");
-  
+
 	copy( companynbr);
 	setStatus(companynbr);
 }
@@ -64,7 +64,7 @@ function getRandomRrnNumber() {
 	var base = ((year%100)*10000000) + (month*100000) + (day*1000) + (counter);
 	var checksum = year>=2000 ? (97 - (base+2000000000) % 97) : (97 - base % 97);
 	var rrn = ''.concat( 100 * base + checksum).padStart(11, "0");
-  
+
 	copy(rrn);
 	setStatus(rrn);
 }
@@ -79,15 +79,10 @@ function getRandomIban() {
     var nationalCheck = (10000000*bankcode + accountNbr)%97==0 ? 97 : (10000000*bankcode + accountNbr)%97;
 	var checksum = 98 - modulo(''+(1000000000000000*bankcode + 100000000*accountNbr + nationalCheck*1000000 + 111400), ''+97);
 	var iban = country.concat(''.concat(1000000000000*checksum + 1000000000*bankcode + 100*accountNbr + nationalCheck).padStart(14,"0"));
-	
+
 	copy(iban);
 	setStatus(iban);
 }
-
-//Telefoonnummer https://nl.wikipedia.org/wiki/Telefoonnummer
-//Kentekenplaat https://nl.wikipedia.org/wiki/Kentekenplaat
-//https://en.wikipedia.org/wiki/Vehicle_identification_number
-
 
 function resetStatus(text) {
     var stat = document.getElementById('status');
@@ -103,7 +98,7 @@ function setStatus(text) {
 	stat.classList.remove("list-group-item-secondary");
 }
 
-function randomIntFromInterval(min, max) { // min and max included 
+function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
