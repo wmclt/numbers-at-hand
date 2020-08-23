c7b930ee78d9b0be60b3ebdd0855ed9a93d465b8
philipdeherdt <philipdeherdt@hotmail.com>
2020-08-23 13:20:32 +0200

    Generate BE IBAN

--- plugin/popup.js
+++ plugin/popup.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var rrnBtn = document.getElementById('btn-rrn');
   var companyBtn = document.getElementById('btn-companynbr');
   var rszBtn = document.getElementById('btn-rsznbr');
+  var ibanBtn = document.getElementById('btn-iban');
   
   uuidBtn.addEventListener('click', function() { getRandomUuid(); }, false);
   rrnBtn.addEventListener('click', function() { getRandomRrnNumber(); }, false);
   companyBtn.addEventListener('click', function() { getRandomCompanyNumber(); }, false);
   rszBtn.addEventListener('click', function() { getRandomRszNumber(); }, false);
+  ibanBtn.addEventListener('click', function() { getRandomIban(); }, false);
+	
 	
   var title = document.getElementById('title');
   
@@ -15,20 +18,21 @@ document.addEventListener('DOMContentLoaded', function() {
   rrnBtn.innerHTML = chrome.i18n.getMessage("nationalIdentificationNumber");
   companyBtn.innerHTML = chrome.i18n.getMessage("companyNumber");
   rszBtn.innerHTML = chrome.i18n.getMessage("rszNumber");
+  ibanBtn.innerHTML = chrome.i18n.getMessage("iban");
   title.innerHTML = chrome.i18n.getMessage("pluginname");
 }, false);
 
 //https://www.uuidgenerator.net/
 function getRandomUuid() {
-	uuid = "00000000-0000-0000-0000-000000000000"
+	var uuid = "00000000-0000-0000-0000-000000000000"
 	copy(uuid);
 	setStatus(uuid);
 }
 //https://nl.wikipedia.org/wiki/RSZ-nummer
 function getRandomRszNumber() {
-	base = randomIntFromInterval(1000,1999999); 
-	control = 96- (100*base)%97;
-	rsznbr = ''.concat( 100 * base + control).padStart(9, "0");
+	var base = randomIntFromInterval(1000,1999999); 
+	var control = 96- (100*base)%97;
+	var rsznbr = ''.concat( 100 * base + control).padStart(9, "0");
   
 	copy(rsznbr);
 	setStatus(rsznbr);
@@ -36,9 +40,9 @@ function getRandomRszNumber() {
 
 //https://nl.wikipedia.org/wiki/Kruispuntbank_van_Ondernemingen
 function getRandomCompanyNumber() {
-	base = randomIntFromInterval(2000000,19999999); 
-	control = 97 - base % 97;
-	companynbr = ''.concat( 100 * base + control).padStart(10, "0");
+	var base = randomIntFromInterval(2000000,19999999); 
+	var control = 97 - base % 97;
+	var companynbr = ''.concat( 100 * base + control).padStart(10, "0");
   
 	copy( companynbr);
 	setStatus(companynbr);
@@ -46,20 +50,34 @@ function getRandomCompanyNumber() {
 
 //https://nl.wikipedia.org/wiki/Rijksregisternummer
 function getRandomRrnNumber() {
-	year = randomIntFromInterval(1920,2019);
-	month = randomIntFromInterval(1,12);
-	day = randomIntFromInterval(1,28);
-	counter = randomIntFromInterval(1,999);
+	var year = randomIntFromInterval(1920,2019);
+	var month = randomIntFromInterval(1,12);
+	var day = randomIntFromInterval(1,28);
+	var counter = randomIntFromInterval(1,999);
 
-	base = ((year%100)*10000000) + (month*100000) + (day*1000) + (counter);
-	checksum = year>=2000 ? (97 - (base+2000000000) % 97) : (97 - base % 97);
-	rrn = ''.concat( 100 * base + checksum).padStart(11, "0");
+	var base = ((year%100)*10000000) + (month*100000) + (day*1000) + (counter);
+	var checksum = year>=2000 ? (97 - (base+2000000000) % 97) : (97 - base % 97);
+	var rrn = ''.concat( 100 * base + checksum).padStart(11, "0");
   
 	copy(rrn);
 	setStatus(rrn);
 }
 
-//IBAN https://nl.wikipedia.org/wiki/International_Bank_Account_Number
+//https://www.iban.com/calculate-iban https://nl.wikipedia.org/wiki/International_Bank_Account_Number
+function getRandomIban() {
+
+	var country = "BE";
+	var bankcode = randomIntFromInterval(0,999); //https://www.nbb.be/en/payments-and-securities/payment-standards/bank-identification-codes
+	var accountNbr = randomIntFromInterval(0,9999999);
+
+    var nationalCheck = (10000000*bankcode + accountNbr)%97==0 ? 97 : (10000000*bankcode + accountNbr)%97;
+	var checksum = 98 - modulo(''+(1000000000000000*bankcode + 100000000*accountNbr + nationalCheck*1000000 + 111400), ''+97);
+	var iban = country.concat(''.concat(1000000000000*checksum + 1000000000*bankcode + 100*accountNbr + nationalCheck).padStart(14,"0"));
+	
+	copy(iban);
+	setStatus(iban);
+}
+
 //Telefoonnummer https://nl.wikipedia.org/wiki/Telefoonnummer
 //Kentekenplaat https://nl.wikipedia.org/wiki/Kentekenplaat
 //https://en.wikipedia.org/wiki/Vehicle_identification_number
@@ -81,4 +99,30 @@ function copy(text) {
     var result = document.execCommand('copy');
     document.body.removeChild(input);
     return result;
+}
+
+function modulo (divident, divisor) {
+    var cDivident = '';
+    var cRest = '';
+
+    for (var i in divident ) {
+        var cChar = divident[i];
+        var cOperator = cRest + '' + cDivident + '' + cChar;
+
+        if ( cOperator < parseInt(divisor) ) {
+                cDivident += '' + cChar;
+        } else {
+                cRest = cOperator % divisor;
+                if ( cRest == 0 ) {
+                    cRest = '';
+                }
+                cDivident = '';
+        }
+
+    }
+    cRest += '' + cDivident;
+    if (cRest == '') {
+        cRest = 0;
+    }
+    return cRest;
 }
