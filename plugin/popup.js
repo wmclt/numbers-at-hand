214516aee1f92fc3e3c4d51b5dba60d5a21b962a
Philip De Herdt <philip.de.herdt@acerta.be>
2020-09-12 11:07:23 +0200

    Removed unused functions

--- plugin/popup.js
+++ plugin/popup.js
@@ -338,22 +338,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // **************************** Helper functions ******************************
 
-
-function readTextFile(file) { //https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", file, false);
-  rawFile.onreadystatechange = function() {
-    if (rawFile.readyState === 4) {
-      if (rawFile.status === 200 || rawFile.status == 0) {
-        var allText = rawFile.responseText;
-        //  alert(allText);
-        return allText;
-      }
-    }
-  }
-  rawFile.send(null);
-}
-
 function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -409,11 +393,4 @@ if (String.prototype.splice === undefined) {
     return this.substring(0, calculatedOffset) +
       text + this.substring(calculatedOffset + removeCount);
   };
-}
-
-function getLang() { //https://stackoverflow.com/questions/673905/best-way-to-determine-users-locale-within-browser
-  if (navigator.languages != undefined)
-    return navigator.languages[0];
-  else
-    return navigator.language;
 }
