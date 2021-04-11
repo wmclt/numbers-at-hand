0efaf99d6cda21d3f4832aa886cef6434c99dfe0
Philip De Herdt <philipdeherdt@hotmail.com>
2021-04-11 13:55:21 +0200

    Undo change in reference to ga.js since mozilla's first code review was wrong

--- plugin/js/common.js
+++ plugin/js/common.js
@@ -329,7 +329,7 @@ _gaq.push(['_trackPageview']);
   var ga = document.createElement('script');
   ga.type = 'text/javascript';
   ga.async = true;
-  ga.src = 'js/ga/ga.js'; //Original: 'https://ssl.google-analytics.com/ga.js';
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(ga, s);
 })();
